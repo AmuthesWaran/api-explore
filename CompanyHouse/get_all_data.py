f6d5163d14a33fed8dbaac52347ab3f829d5c9ce
amutheswaran <amutheswaran@outlook.com>
2023-12-21 18:02:44 +0530

    get all done. to be optimized

--- CompanyHouse/get_all_data.py
+++ CompanyHouse/get_all_data.py
@@ -1,12 +1,34 @@
 import requests
 import json
+import csv
+import logging
 
-r = requests.get('http://data.companieshouse.gov.uk/doc/company/12345678.json')
+logging.basicConfig(filename='./logs_cono.txt', filemode='a', format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
+logging.info('start')
 
-# print(r.content)
+input_file_path = 'input.txt'
 
-json_data = json.loads(r.content)
+with open(input_file_path, 'r') as file:
+    lines = file.readlines()
 
-print(type(json_data))
+cleaned_line = [line.strip() for line in lines]
 
-print(json_data["primaryTopic"]["CompanyName"])
+# print(cleaned_line)
+# print(type(cleaned_line))
+
+file_name = 'output.csv'
+
+with open(file_name, mode='w', newline='') as file:
+    writer = csv.writer(file, delimiter='|')  # Create a CSV writer object
+    writer.writerow(['CompanyNumber','CompanyName','CompanyCategory','CompanyStatus','SicText']) # Writing header
+
+    for cono in cleaned_line:
+        logging.info(f'cono {cono}')
+        r = requests.get(f'http://data.companieshouse.gov.uk/doc/company/{cono}.json')
+        json_data = json.loads(r.content)
+        # print(type(json_data))
+        # print(json_data["primaryTopic"]["CompanyNumber"],json_data["primaryTopic"]["CompanyName"],json_data["primaryTopic"]["CompanyCategory"],json_data["primaryTopic"]["CompanyStatus"],json_data["primaryTopic"]["SICCodes"]["SicText"])
+        writer.writerow([json_data["primaryTopic"]["CompanyNumber"],json_data["primaryTopic"]["CompanyName"],json_data["primaryTopic"]["CompanyCategory"],json_data["primaryTopic"]["CompanyStatus"],json_data["primaryTopic"]["SICCodes"]["SicText"]])
+
+logging.info('end')
