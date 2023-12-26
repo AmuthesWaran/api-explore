4f0fc772d83b8bb8c5453733058dcad34cabddc3
Amutheswaran <amutheswaran@outlook.com>
2023-12-26 18:31:34 +0530

    exceptions added

--- CompanyHouse/get_all_data.py
+++ CompanyHouse/get_all_data.py
@@ -7,6 +7,7 @@ logging.basicConfig(filename='./logs_cono.txt', filemode='a', format='%(asctime)
                     level=logging.INFO)
 logging.info('start')
 
+# input file must contain only cono in a single column
 input_file_path = 'input.txt'
 
 with open(input_file_path, 'r') as file:
@@ -14,21 +15,23 @@ with open(input_file_path, 'r') as file:
 
 cleaned_line = [line.strip() for line in lines]
 
-# print(cleaned_line)
-# print(type(cleaned_line))
-
 file_name = 'output.csv'
+error_log = 'error_log_all'
 
-with open(file_name, mode='w', newline='') as file:
-    writer = csv.writer(file, delimiter='|')  # Create a CSV writer object
-    writer.writerow(['CompanyNumber','CompanyName','CompanyCategory','CompanyStatus','SicText']) # Writing header
+with open(error_log, mode='w', newline='') as exfile:
+    csv_writer = csv.writer(exfile, delimiter='|')
+    csv_writer.writerow(['CONO','StatusCode'])
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file, delimiter='|')  # Create a CSV writer object
+        writer.writerow(['CompanyNumber','CompanyName','CompanyCategory','CompanyStatus','SicText']) # Writing header
 
-    for cono in cleaned_line:
-        logging.info(f'cono {cono}')
-        r = requests.get(f'http://data.companieshouse.gov.uk/doc/company/{cono}.json')
-        json_data = json.loads(r.content)
-        # print(type(json_data))
-        # print(json_data["primaryTopic"]["CompanyNumber"],json_data["primaryTopic"]["CompanyName"],json_data["primaryTopic"]["CompanyCategory"],json_data["primaryTopic"]["CompanyStatus"],json_data["primaryTopic"]["SICCodes"]["SicText"])
-        writer.writerow([json_data["primaryTopic"]["CompanyNumber"],json_data["primaryTopic"]["CompanyName"],json_data["primaryTopic"]["CompanyCategory"],json_data["primaryTopic"]["CompanyStatus"],json_data["primaryTopic"]["SICCodes"]["SicText"]])
+        for cono in cleaned_line:
+            logging.info(f'cono {cono}')
+            r = requests.get(f'http://data.companieshouse.gov.uk/doc/company/{cono}.json')
+            if r.status_code == 200:
+                json_data = json.loads(r.content)
+                writer.writerow([json_data["primaryTopic"]["CompanyNumber"],json_data["primaryTopic"]["CompanyName"],json_data["primaryTopic"]["CompanyCategory"],json_data["primaryTopic"]["CompanyStatus"],json_data["primaryTopic"]["SICCodes"]["SicText"]])
+            else:
+                csv_writer.writerow([cono,r.status_code])
 
 logging.info('end')
